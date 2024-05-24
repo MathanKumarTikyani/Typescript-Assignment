51e4e3aa704aab926f6e2e54a3a35e4d344b7eb5
MathanKumarTikyani <165132465+MathanKumarTikyani@users.noreply.github.com>
2024-05-24 11:04:39 +0500

    Add files via upload

---
+++ 04.Famous_Quote.ts
@@ -0,0 +1,11 @@
+//04• Famous Quote: 
+
+//• Find a quote from a famous person you admire. 
+
+let authorName : string = ('Aristotle');
+
+let  Quotation : string =('Knowing yourself is the beginning of all wisdom.');
+
+//• Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+console.log(`${authorName} once said, "${Quotation}"`);
