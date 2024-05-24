a3fc3257351eaf6ee4628e0c62009437dca89793
MathanKumarTikyani <165132465+MathanKumarTikyani@users.noreply.github.com>
2024-05-24 11:54:21 +0500

    Add files via upload

---
+++ 13.Favourite_Transportation.ts
@@ -0,0 +1,12 @@
+//13• Your Own Array: 
+
+//• Think of your favorite mode of transportation, Such as a motorcycle or a car, and make a list that stores several examples. 
+
+let transportation : String[] = ['Car','Motorcycle','Train','Cycle'];
+
+//• Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.
+
+transportation.forEach(transportation => {
+    console.log(`"I would like to own a ${transportation}"`);
+})
+
