dbe423089054cdc01544cd300f4f9ba28d4866d4
MathanKumarTikyani <165132465+MathanKumarTikyani@users.noreply.github.com>
2024-05-24 11:32:52 +0500

    Add files via upload

---
+++ 10.Adding_Comments.ts
@@ -0,0 +1,26 @@
+//10• Adding Comments: 
+
+//• Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+// Program 1.
+// program written by Mathan Kumar.
+// Current Date : 23/2/24.
+
+// This program stores a favorite number and message in a variable and also concatenates both variables.
+
+const favourite_Number : number = 5;
+
+const message  = "Here is your favourite number" + " " + favourite_Number ;
+
+console.log(message);
+
+// Two
+// program written by Mathan Kumar.
+// Current Date : 23/2/24.
+
+// This programs shows the mathematical operation like Addition,subtraction,multiplication and division.Each statement answer is 8.
+
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 *2);
+console.log(80 / 10);
