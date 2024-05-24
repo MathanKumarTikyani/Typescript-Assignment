d8e3199694cd9f3199998229cf724fa2cb390878
MathanKumarTikyani <165132465+MathanKumarTikyani@users.noreply.github.com>
2024-05-24 11:06:59 +0500

    Add files via upload

---
+++ 05.Famous_Quote_2.ts
@@ -0,0 +1,13 @@
+//05• Famous Quote 2: Repeat Exercise 4. 
+
+//• This time store the famous person’s name in a variable called famous_person.
+
+let famous_person : String = "Albert Einstein";
+
+//• Then compose your message and store it in a new variable called message. 
+
+let message : String = "Imagination is more important than knowledge.";
+
+//• Print your message.5.
+
+console.log(`${famous_person} once said, "${message}"`);
