678b8ab252901ab2b6d55f8484961723ed2f263d
MathanKumarTikyani <165132465+MathanKumarTikyani@users.noreply.github.com>
2024-05-24 10:58:48 +0500

    Add files via upload

---
+++ 03.Name_Cases.ts
@@ -0,0 +1,19 @@
+//03• Name Cases: 
+
+//• Store a person’s name in a variable.
+
+let personName : String = "Mathan Kumar";
+
+//• Print that person’s name in lowercase.
+console.log("Lowercase:", personName.toLowerCase());
+
+//• Print that person name in uppercase.
+console.log("Uppercase:", personName.toUpperCase());
+
+//• Print that person name in titlecase.
+console.log("Titlecase:", personName[0].toUpperCase() + personName.slice(1).toLowerCase());
+
+
+
+
+
