81c6de8fad0d9bcd8c3d969f0df6d88eac2140d2
MathanKumarTikyani <165132465+MathanKumarTikyani@users.noreply.github.com>
2024-05-28 10:20:41 +0500

    Add files via upload

---
+++ 20.Mountain.ts
@@ -0,0 +1,15 @@
+//20• Mountain:
+
+//• Think of something you could store in a array.
+
+let Mountain : String[] = ['K2','Nanga Parbat','Mount Everest'];
+
+//• Write a program that creates a list containing these items.
+
+let items : String[] = [];
+
+items.push('K2');
+items.push('Nanga Parbat');
+items.push('Mount Everest');
+
+console.log(items);
