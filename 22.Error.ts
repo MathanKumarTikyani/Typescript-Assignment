7f16e8d8f4a046ce32c921db6337790b37c0a70c
MathanKumarTikyani <165132465+MathanKumarTikyani@users.noreply.github.com>
2024-05-28 10:38:29 +0500

    Add files via upload

---
+++ 22.Error.ts
@@ -0,0 +1,14 @@
+//22• Intentional Error:  
+
+//• If you have not received an array index error in one of your program yet , try to make one happen.
+
+//• Creating array
+let colorName_Array : String[] = ['Red','Blue','Pink'];
+
+//• Accessing array by index.
+
+console.log(colorName_Array[5]);
+
+//• Make sure you correct the error before closing the program.
+//• Correct the error.
+console.log(colorName_Array);
