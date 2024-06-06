70de2cd9834b1afc3743e078240d4298dcf362a7
MathanKumarTikyani <165132465+MathanKumarTikyani@users.noreply.github.com>
2024-06-06 10:23:10 +0500

    Add files via upload

---
+++ 33.Ordinal_Number.ts
@@ -0,0 +1,33 @@
+ //33• Ordinal Numbers: 
+ 
+ //• Ordinal numbers Indicate their position in a array such as 1st or 2nd. Most ordinal number end in th, except 1,2 and 3.
+
+  //• Store the numbers 1 through 9 in a array.
+
+let storeNumbers : number[] = [1,2,3,4,5,6,7,8,9];
+
+   //• Loop through the array.
+
+ for (let num of storeNumbers ) {
+
+  let ordinalEnding : string;
+
+ //• Use an if else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th and each result should be on a separate line.
+
+  if (num === 1) {
+    ordinalEnding = "st";
+
+  } else if (num === 2 ) {
+     ordinalEnding = "nd";
+
+  } else if(num === 3) {
+    ordinalEnding = "rd";
+
+  } else {
+    ordinalEnding = "th"
+  }
+
+  console.log(`${num}${ordinalEnding}`);
+
+ }
+
